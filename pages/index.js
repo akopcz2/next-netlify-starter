c66a66edf6d947c0c40d0f0e895644d4a2c673b0
akopcz2 <akopcz2@gmail.com>
2023-05-21 21:55:35 -0500

    Adding things

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,48 @@
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
+import styled from 'styled-components';
+
+import bg from '../public/farmland.jpg'
+
+const Wrapp = styled.div`
+
+    &::after{
+        display: block;
+        content: '';
+        background-image: url(${bg.src});
+        width: 100%;
+        height: 100%;
+        position: absolute;
+        left: 0;
+        top: 0;
+        background-size: cover;
+        z-index: 0;
+        opacity: 0.2;
+    }
+
+`
+
 
 export default function Home() {
+  
   return (
-    <div className="container">
-      <Head>
-        <title>KOPDENFARMS.COM</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <div>
+      <Wrapp>
+        <Head>
+          <title>KOPDENFARMS.COM</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
 
-      <main>
-        <Header title="KOPDENFARMS.COM" />
-        <p className="description">
-          <code>EST. 2023</code>
-        </p>
-      </main>
+        <main style={{"height":"100vh"}}>
+          <Header title="KOPDENFARMS.COM" />
+          <p>
+            <code>EST. 2023</code>
+          </p>
+        </main>
 
-      <Footer />
+      {/* <Footer /> */}
+      </Wrapp>
     </div>
   )
 }
