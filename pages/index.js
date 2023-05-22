64d24a9431cd705e6707a780d8430b294ae297e6
akopcz2 <akopcz2@gmail.com>
2023-05-21 20:25:20 -0500

    Init

--- pages/index.js
+++ pages/index.js
@@ -11,9 +11,9 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="KOPDENFARMS.COM" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          <code>EST. 2023</code>
         </p>
       </main>
 
